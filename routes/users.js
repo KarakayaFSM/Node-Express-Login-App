50236a3c9227c4b3c23678e41540902c5ff06f08
Karakaya <salih.karakaya@stu.fsm.edu.tr>
2019-09-21 18:21:17 +0300

    implemented password hashing and login functionality

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,19 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
 
 const router = express.Router();
+
+//User Model
+const User = require('../models/User');
 
 //Login Page
 router.get('/login', (req, res) => {
     res.render('login');
 });
 
-
+router.post('/login', (req, res) => {
+    res.render('dashboard',{user:req.body});
+});
 
 //Register Page
 router.get('/register', (req, res) => {
@@ -45,8 +51,41 @@ router.post('/register', (req, res) => {
             name,
             email
         });
+        return;
     }
+        User.findOne({
+            email: email
+        })
+        .then((user) => {
+            if (user) {
+                errors.push({
+                    msg: 'Email is already registered'
+                })
+                // ES6 Object initialization (instead of name:name, email:email)
+                res.render('register', {
+                    errors,
+                    name,
+                    email
+                });
+                return;
+            } 
+                const newUser = new User({
+                    name,
+                    email,
+                    password
+                });
+                //Encrypt Password With Bcrypt
+                bcrypt.genSalt(10,(err,salt) =>
+                    bcrypt.hash(newUser.password,salt, (err,hash) =>{
+                        if(err) throw err;
+                        //Set password to hash
+                        newUser.password = hash;
 
+                        newUser.save()
+                        .then((user) => res.redirect('/users/login'))
+                            .catch((err) => console.log(err))
+                }));
+        })
 });
 
 
